4f04fce978c1c0afc17e400c026a29b19b84eabc
Dinapoli <68258030+dinapolicv@users.noreply.github.com>
2022-09-19 16:55:38 -0400

    Update index.jsx

--- src/Components/Rodape/index.jsx
+++ src/Components/Rodape/index.jsx
@@ -1,8 +1,9 @@
 export function Rodape (){
     return(
         <div className="rodape">
-            <h1 className="es">Boca Web</h1>
-            <p className="es">Dinapoli</p>
+            <h1 className="es"> Boca Web </h1>
+            <p className="es"> IFMS Dourados – Trabalho de Frameworks2 </p>
+            <p className="es"> Aluno: Dinapoli </p>
         </div>
     )
 }
